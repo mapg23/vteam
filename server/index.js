a0ba81d779b409008bb62cb13b9f9557151f76a7
mapg23 <mapg23@student.bth.se>
2024-12-18 12:03:36 +0100

    fixed workflow & server routes

--- server/index.js
+++ server/index.js
@@ -13,11 +13,38 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/customer", customer_route);
 app.use("/admin", admin_route);
 
+app.listen(port, logStartUpDetailsToConsole);
+
 app.get("/", (req, res) => {
     // res.redirect("/customer");
     res.send("<h1>Rebuilt</h1>")
 });
 
-app.listen(port, () => {
-    console.log(`Server started at port: ${port}`);
-}); 
+/**
+ * Log app details to console when starting up.
+ *
+ * @return {void}
+ */
+function logStartUpDetailsToConsole() {
+    let routes = [];
+
+    // Find what routes are supported
+    app._router.stack.forEach((middleware) => {
+        if (middleware.route) {
+            // Routes registered directly on the app
+            routes.push(middleware.route);
+        } else if (middleware.name === "router") {
+            // Routes added as router middleware
+            middleware.handle.stack.forEach((handler) => {
+                let route;
+
+                route = handler.route;
+                route && routes.push(route);
+            });
+        }
+    });
+
+    console.info(`Server is listening on port ${port}.`);
+    console.info("Available routes are:");
+    console.info(routes);
+}
