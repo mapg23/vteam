8e81d56c35d3d96b7aceaba3cc56ace8cfd88da4
mapg23 <mapg23@student.bth.se>
2024-12-18 13:29:44 +0100

    docker integration test

--- server/routes/admin_route.js
+++ server/routes/admin_route.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("<h1>Hello admin route</h1>")
+    res.send("<h1>Hello admin Route</h1>")
 });
 
 module.exports = router;
